592d0bd7ae91ef95b2897bef2c5a8fb4a29e378f
Dirk <so0mewhat2@gmail.com>
2025-10-29 09:51:52 +0100

    added analytics

--- README.md
+++ README.md
@@ -2,15 +2,6 @@
 
 ## TODO
 
-- [x] Set up database and data model
-- [x] Move folder open state to URL
-- [x] Add auth
-- [x] Add file uploading
 - [ ] Add analytics
-
-## 28-10-2025
-
-- [x] Upload files to the correct folder
-- [x] Add "ownership" to files and folders
-- [ ] Add homepage
+- [ ] Add homepage & onboarding
 - [ ] Delete files button
---
+++ src/app/_providers/posthog-provider.tsx
@@ -0,0 +1,34 @@
+// app/providers.tsx
+"use client";
+
+import { useEffect } from "react";
+import posthog from "posthog-js";
+import { PostHogProvider as PHProvider } from "posthog-js/react";
+import { env } from "~/env";
+import dynamicLoader from "next/dynamic";
+
+const SuspendedPostHogPageView = dynamicLoader(
+  () => import("./pageview-tracker"),
+  {
+    ssr: false,
+  },
+);
+
+export function PostHogProvider({ children }: { children: React.ReactNode }) {
+  useEffect(() => {
+    posthog.init(env.NEXT_PUBLIC_POSTHOG_KEY as string, {
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+      api_host: "/ingest",
+      ui_host: "https://us.posthog.com",
+      person_profiles: "identified_only", // or 'always' to create profiles for anonymous users as well
+      defaults: "2025-05-24",
+    });
+  }, []);
+
+  return (
+    <PHProvider client={posthog}>
+      <SuspendedPostHogPageView />
+      {children}
+    </PHProvider>
+  );
+}
